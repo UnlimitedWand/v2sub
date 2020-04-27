5a3d2d7f9068d1015f335a0708663a84ab1c51aa
Blade <unlimitedblade@protonmail.com>
2020-04-27 12:14:07 +0800

    rewrite the code

--- v2sub.py
+++ v2sub.py
@@ -8,9 +8,10 @@ import base64
 import json
 import subprocess
 import re
+import signal
 try: 
     import socks 
-except ImportError: 
+except ImportError:
     print("未安装模块Pysocks!现在开始安装...")
     subprocess.call('pip3 install pysocks', shell = True)
     print("请重新运行程序")
@@ -21,221 +22,268 @@ if os.geteuid() != 0:
     print("您需要切换到 Root 身份才可以使用本脚本。尝试在命令前加上 sudo?\n")
     exit()
 
-# 本脚本的配置文件，目前的作用是仅存储用户输入的订阅地址，这样用户再次启动脚本时，就无需再输入订阅地址。
-# 预设的存储的路径为存储到用户的 HOME 内。
-subFilePath = os.path.expandvars('$HOME') + '/.v2sub.conf'
-subProxyPath = os.path.expandvars('$HOME') + '/.v2sub_proxy.conf' #获取订阅信息所用代理配置
-# 获取订阅地址
-if not os.path.exists(subFilePath):
-    os.mknod(subFilePath)
+class v2sub():
+    def __init__(self):
+        self.currentVersion = 1.0
+        # 本脚本的配置文件，目前的作用是仅存储用户输入的订阅地址，这样用户再次启动脚本时，就无需再输入订阅地址。
+        # 预设的存储的路径为存储到用户的 HOME 内。
+        self.subFilePath = os.path.expandvars('$HOME') + '/.v2sub.conf'
+        self.subBackupPath = os.path.expandvars('$HOME') + '/.v2sub.conf.backup'
+        self.subLinks = []
+        self.proxy = 'socks5://127.0.0.1:10808'
+        self.currentNode = ''
+        self.version = self.currentVersion
+        self.serverListLink = []
+        self.setServerNodeId = 0
 
-subFile = open(subFilePath, 'r')
-subLinks = subFile.readlines()
-subFile.close()
+    def write_v2sub_conf(self):
+        # v2sub 配置文件, 不要轻易修改！
+        v2subConf = {
+            "version": self.currentVersion,
+            "subLinks": self.subLinks,
+            "proxy": self.proxy,
+            "currentNode": self.currentNode
+        }
+        json.dump(v2subConf, open(self.subFilePath, 'w'), indent=2)
+    #end of write_v2sub_conf
 
-while True:
-    if not subLinks:
-        print('\n您还没有输入订阅地址，请输入订阅地址。')
-        subLink = input('订阅地址：')
-        subFile = open(subFilePath, 'w+')
-        subFile.write(subLink)
-        subFile.seek(0,0);
-        subLinks = subFile.readlines()
-        subFile.close()
-    print("\n当前订阅：")
-    for i in range(len(subLinks)):
-        subLinks[i]= subLinks[i].strip()
-        print('订阅链接'+'【' + str(i) + '】：'+subLinks[i])
-    choose = input('\n1.更换节点 2.增加订阅链接 3.删除订阅链接 , 请输入（输入q退出）：')
-    if choose == 'q':
-        exit()
-    if choose == '1':
-        break    
-    if choose == '2':
-        subLink = input('订阅地址：')            
-        subFile = open(subFilePath, 'a+')
-        subFile.write('\n'+subLink)
-        subFile.seek(0,0);
-        subLinks = subFile.readlines()
-        subFile.close()
 
-    if choose == '3':
-        while True:
-            todeleID = input("请输入要删除的订阅编号：")
-            if todeleID.isdigit():
-                todeleID = int(todeleID)
+    def write_v2ray_conf(self):
+        # 编辑 v2ray 配置文件
+        v2rayConf = {
+          "inbound": {
+            "port": 10808,
+            "listen": "127.0.0.1",
+            "protocol": "socks",
+        	"sniffing": {
+                "enabled": True,
+                "destOverride": [
+                  "http",
+                  "tls"
+                ]
+            },
+        	"settings": {
+        		"auth": "noauth",
+                "udp": True,
+        		"ip": "127.0.0.1"
+            }
+          },
+          "outbound": {
+            "tag": "proxy",
+            "protocol": "vmess",
+            "settings":{ 
+                "vnext": [
+                  {
+                    "address": self.serverListLink[self.setServerNodeId]['add'],
+                    "port": int(self.serverListLink[self.setServerNodeId]['port']),
+                    "users": [
+                      {
+                        "id": self.serverListLink[self.setServerNodeId]['id'],
+                        "alterId": int(self.serverListLink[self.setServerNodeId]['aid']),
+        				"security": "auto"
+                      }
+                    ]
+                  }
+                ]
+              },
+              "streamSettings": {
+                "network": self.serverListLink[self.setServerNodeId]['net'],
+                "security": self.serverListLink[self.setServerNodeId]['tls'],
+                "tlsSettings": {
+                  "allowInsecure": False,
+                  "serverName": self.serverListLink[self.setServerNodeId]['host']
+                },
+                "wsSettings": {
+                  "connectionReuse": True,
+                  "path": self.serverListLink[self.setServerNodeId]['path'],
+                  "headers": {
+                    "Host": self.serverListLink[self.setServerNodeId]['host']
+                  }
+                }
+              },
+              "mux": {
+                "enabled": False,
+                "concurrency": -1
+              }
+            },
+          "outboundDetour": [{
+            "protocol": "freedom",
+            "tag": "direct",
+            "settings": {}
+          }],
+          "routing": {
+            "strategy": "rules",
+            "settings": {
+              "domainStrategy": "IPOnDemand",
+              "rules": [{
+                "type": "field",
+                "ip": [
+                  "0.0.0.0/8",
+                  "10.0.0.0/8",
+                  "100.64.0.0/10",
+                  "127.0.0.0/8",
+                  "169.254.0.0/16",
+                  "172.16.0.0/12",
+                  "192.0.0.0/24",
+                  "192.0.2.0/24",
+                  "192.168.0.0/16",
+                  "198.18.0.0/15",
+                  "198.51.100.0/24",
+                  "203.0.113.0/24",
+                  "::1/128",
+                  "fc00::/7",
+                  "fe80::/10"
+                ],
+                "outboundTag": "direct"
+              }]
+            }
+          }
+        }
+        json.dump(v2rayConf, open('/etc/v2ray/config.json', 'w'), indent=2)
+    #end of write_v2ray_conf
+
+    #获取节点信息
+    def get_nodes(self):
+        while True:    
+            num = input('选择订阅链接: ')
+            if num.isdigit():
+                num = int(num)
+                if num not in range(len(self.subLinks)):
+                    print("输入的编号不存在！\n")
+                    continue
+                break
             else:
                 print("只能输入数字！")
                 continue
-            if todeleID not in range(len(subLinks)):
-                print("输入的编号不存在！\n")
-                continue
-            if re.search('[yesYES]', input('确定要删除该订阅吗？[y/N] ')):
+        subLink = self.subLinks[num]
+        proxy_flag=False
+        proxy_old = "socks5://127.0.0.1:10808"
+        if re.search('[yesYES]', input('通过代理获取订阅信息？[y/N] ,默认N：')):
+            proxy_flag=True    
+            if self.proxy:
+                proxy_old = self.proxy
+            proxy_new = input('默认代理地址为'+proxy_old+'。请输入地址：')
+            if proxy_new and proxy_new !=  proxy_old:
+                proxy = proxy_new
+            else:
+                proxy = proxy_old
+            proxies = {'http': proxy, 'https': proxy}
+        print("\n开始从订阅地址中读取服务器节点… 如等待时间过久，请检查网络或使用代理。\n")
+        # 获取订阅信息
+        try:
+            if proxy_flag:
+                self.serverListLink = base64.b64decode(requests.get(subLink,proxies=proxies).text + "===").splitlines()
+                self.proxy = proxy
+            else:
+                self.serverListLink = base64.b64decode(requests.get(subLink).text + "===").splitlines()
+        except Exception as e:
+            print("订阅地址或代理地址无法连接! 请重新选择或填写.\n异常信息："+str(e))
+            return False
+        self.write_v2sub_conf()
+        for i in range(len(self.serverListLink)):
+            serverNode = json.loads(bytes.decode(base64.b64decode(bytes.decode(self.serverListLink[i]).replace('vmess://',''))))
+            print('【' + str(i) + '】' + serverNode['ps'])
+            self.serverListLink[i] = serverNode
+        return True
+    #end of get_nodes
+
+    #切换节点
+    def change_node(self):
+        while True:    
+            self.setServerNodeId = input("\n请输入要切换的节点编号：")
+            if self.setServerNodeId.isdigit():
+                self.setServerNodeId = int(self.setServerNodeId)
+                if self.setServerNodeId not in range(len(self.serverListLink)):
+                    print("输入的编号不存在！\n")
+                    continue
                 break
-        del(subLinks[todeleID])
-        subFile = open(subFilePath, 'w')
-        subFile.writelines(line + '\n' for line in subLinks)
-        subFile.close()
-
-
-while True:    
-    link = input('选择订阅链接: ')
-    if link.isdigit():
-        link = int(link)
-        if link not in range(len(subLinks)):
-            print("输入的编号不存在！\n")
-            continue
-        break
-    else:
-        print("只能输入数字！")
-        continue
-subLink = subLinks[link]
-proxy_flag=False
-proxy_old = "socks5://127.0.0.1:10808"
-if re.search('[yesYES]', input('通过代理获取订阅信息？[y/N] ,默认N：')):
-    proxy_flag=True
-    if not os.path.exists(subProxyPath):
-        os.mknod(subProxyPath)
-    proxyFile = open(subProxyPath, 'r')
-    proxy = proxyFile.readline()
-    proxyFile.close()    
-    if proxy:
-        proxy_old = proxy
-    proxy_new = input('默认代理地址为'+proxy_old+'。请输入地址：')
-    '''if re.search('[socks5]', proxy_new):
-        subprocess.call('pip3 install pysocks', shell = True)
-    '''
-    if proxy_new and proxy_new !=  proxy_old:
-        proxyFile = open(subProxyPath, 'w')
-        proxy = proxyFile.write(proxy_new)
-        proxyFile.close()
-        proxy = proxy_new
-    else:
-        proxy = proxy_old
-    proxies = {'http': proxy, 'https': proxy}
-print("\n开始从订阅地址中读取服务器节点… 如等待时间过久，请检查网络。\n")
-# 获取订阅信息
-if proxy_flag:
-    serverListLink = base64.b64decode(requests.get(subLink,proxies=proxies).text + "===").splitlines()
-else:
-    serverListLink = base64.b64decode(requests.get(subLink).text + "===").splitlines()
-for i in range(len(serverListLink)):
-    serverNode = json.loads(bytes.decode(base64.b64decode(bytes.decode(serverListLink[i]).replace('vmess://',''))))
-    print('【' + str(i) + '】' + serverNode['ps'])
-    serverListLink[i] = serverNode
-
-while True:
-    while True:    
-        setServerNodeId = input("\n请输入要切换的节点编号：")
-        if setServerNodeId.isdigit():
-            setServerNodeId = int(setServerNodeId)
-            if setServerNodeId not in range(len(serverListLink)):
-                print("输入的编号不存在！\n")
+            else:
+                print("只能输入数字！")
                 continue
-            break
-        else:
-            print("只能输入数字！")
-            continue
-    subprocess.call('ping ' + serverListLink[setServerNodeId]['add'] + ' -c 3 -w 10', shell = True)
-    if re.search('[yesYES]', input('确定要使用该节点吗？[y/N] ')):
-        break
+        subprocess.call('ping ' + self.serverListLink[self.setServerNodeId]['add'] + ' -c 3 -w 10', shell = True)
+        if re.search('[yesYES]', input('确定要使用该节点吗？[y/N] ')):
+            self.currentNode = self.serverListLink[self.setServerNodeId]['ps']
+            self.write_v2ray_conf()
+            print("\n重启 v2ray 服务……\n")
+            subprocess.call('systemctl restart v2ray.service', shell = True)
+            print('地址切换完成')
+    #end of change_node
 
-# 编辑 v2ray 配置文件
-v2rayConf = {
-  "inbound": {
-    "port": 10808,
-    "listen": "127.0.0.1",
-    "protocol": "socks",
-	"sniffing": {
-        "enabled": True,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-    },
-	"settings": {
-		"auth": "noauth",
-        "udp": True,
-		"ip": "127.0.0.1"
-    }
-  },
-  "outbound": {
-    "tag": "proxy",
-    "protocol": "vmess",
-    "settings":{ 
-        "vnext": [
-          {
-            "address": serverListLink[setServerNodeId]['add'],
-            "port": int(serverListLink[setServerNodeId]['port']),
-            "users": [
-              {
-                "id": serverListLink[setServerNodeId]['id'],
-                "alterId": int(serverListLink[setServerNodeId]['aid']),
-				"security": "auto"
-              }
-            ]
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": serverListLink[setServerNodeId]['net'],
-        "security": serverListLink[setServerNodeId]['tls'],
-        "tlsSettings": {
-          "allowInsecure": False,
-          "serverName": serverListLink[setServerNodeId]['host']
-        },
-        "wsSettings": {
-          "connectionReuse": True,
-          "path": serverListLink[setServerNodeId]['path'],
-          "headers": {
-            "Host": serverListLink[setServerNodeId]['host']
-          }
-        }
-      },
-      "mux": {
-        "enabled": False,
-        "concurrency": -1
-      }
-    },
-  "outboundDetour": [{
-    "protocol": "freedom",
-    "tag": "direct",
-    "settings": {}
-  }],
-  "routing": {
-    "strategy": "rules",
-    "settings": {
-      "domainStrategy": "IPOnDemand",
-      "rules": [{
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "127.0.0.0/8",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "direct"
-      }]
-    }
-  }
-}
+    def load_config_file(self):
+        # 获取订阅地址
+        if not os.path.exists(self.subFilePath):
+            os.mknod(self.subFilePath)
+        
+        subFile = open(self.subFilePath, 'r')
+        config_json = subFile.read()
+        subFile.close()
+        
+        try:
+            config = json.loads(config_json)
+            self.subLinks = config['subLinks']
+            self.proxy = config['proxy']
+            self.currentNode = config['currentNode']
+            self.version = config["version"]
+        except Exception as e:
+            print('\n配置文件解析异常或无配置文件，将重新创建...\n异常信息："+str(e)')
+            if os.path.exists(self.subFilePath):
+                print('旧配置文件保存在' + self.subBackupPath)
+                os.rename(self.subFilePath,self.subBackupPath)
+                os.mknod(self.subFilePath)
+                subFile = open(self.subFilePath, 'r')
+                config_json = subFile.read()
+                subFile.close()
+    #end of load_config_file
 
-json.dump(v2rayConf, open('/etc/v2ray/config.json', 'w'), indent=2)
+    def main(self):
+        self.load_config_file()
+        while True:
+            if not self.subLinks:
+                print('\n您还没有输入订阅地址，请输入订阅地址。')
+                subLink = input('订阅地址：')
+                self.subLinks.append(subLink)
+            print("\n当前节点：" + self.currentNode)
+            for i in range(len(self.subLinks)):
+                self.subLinks[i]= self.subLinks[i].strip()
+                print('订阅链接'+'【' + str(i) + '】：'+self.subLinks[i])
+            choose = input('\n1.更换节点 2.增加订阅链接 3.删除订阅链接 , 请输入（输入q退出）：')
+            if choose == 'q':
+                exit()
+            
+            #1.更换节点
+            if choose == '1':
+                if self.get_nodes():
+                    self.change_node()
+            #2.增加订阅链接
+            if choose == '2': 
+                subLink = input('订阅地址：')            
+                self.subLinks.append(subLink)
+                self.write_v2sub_conf()
+                continue
+            #3.删除订阅链接
+            if choose == '3':
+                while True:
+                    todeleID = input("请输入要删除的订阅编号：")
+                    if todeleID.isdigit():
+                        todeleID = int(todeleID)
+                    else:
+                        print("只能输入数字！")
+                        continue
+                    if todeleID not in range(len(self.subLinks)):
+                        print("输入的编号不存在！\n")
+                        continue
+                    if re.search('[yesYES]', input('确定要删除该订阅吗？[y/N] ')):
+                        break
+                del(self.subLinks[todeleID])
+                self.write_v2sub_conf()
+    #end of main
 
-print("\n重启 v2ray 服务……\n")
-subprocess.call('systemctl restart v2ray.service', shell = True)
+def v2sub_exit(signum, frame):
+  print('\nProgram quit.')
+  sys.exit()
 
-print('地址切换完成')
-
-exit()
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, v2sub_exit)
+    signal.signal(signal.SIGTERM, v2sub_exit)    
+    sub=v2sub()
+    sys.exit(sub.main())
